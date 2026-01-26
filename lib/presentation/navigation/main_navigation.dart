5156541cf1e5cd02d1b5c36bcfe6c637a979b4ec
Infvyr <allfortrashmail@gmail.com>
2026-01-26 14:58:51 +0200

    WIP: fix order screens

--- lib/presentation/navigation/main_navigation.dart
+++ lib/presentation/navigation/main_navigation.dart
@@ -16,8 +16,8 @@ class _MainNavigationState extends State<MainNavigation> {
 
   final List<Widget> _screens = [
     const ExpensesScreen(),
-    const StatsScreen(),
     const CategoriesScreen(),
+    const StatsScreen(),
     const SettingsScreen(),
   ];
 
